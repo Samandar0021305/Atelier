06ecee31d2d43fb2fa7892d6090d36000aed71e1
Samandar0021305 <samandar021325@gmail.com>
2022-09-18 11:43:53 +0500

    init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+window.addEventListener("DOMContentLoaded", () => {
+    // LOADER
+    const loader = document.querySelector(".loader");
+    setTimeout(function () {
+      loader.style.opacity = 0;
+      setTimeout(function () {
+        loader.style.display = "none";
+      }, 1500);
+    }, 2000);
+  })
